097ea168dce36537302379f959240308c19ee53d
Rinaldo <nikilson30@gmail.com>
2021-12-29 00:45:01 +0530

    Login Partially completed

--- timecounter.py
+++ timecounter.py
@@ -10,6 +10,9 @@ top.geometry("700x450")
 #    relief="flat")
 def font_style():
     print("HI")
+def my_print():
+    global my_input
+    print(my_input.get())
 welcome_open_encrypter = tkinter.Label(top, text="Welcome to Open Encrypter!!!\n",
     relief="flat", font=("Arial", 22))
 welcome_open_encrypter.pack()
@@ -21,6 +24,12 @@ home_b3 = tkinter.Button(top, width=30, padx=2, pady=2, font=("Arial", 18), text
 home_b3.pack()
 home_b4 = tkinter.Button(top, width=30, padx=2, pady=2, font=("Arial", 18), text="Quit", command=lambda: print("quit"))
 home_b4.pack()
-# Finish
-
+my_input = tkinter.Entry(top, width=30, font=("Arial", 18), show="*")
+my_input.pack()
+home_b5 = tkinter.Button(top, width=30, padx=2, pady=2, font=("Arial", 18), text="Print", command=lambda: my_print())
+home_b5.pack()
+my_pass = tkinter.Entry(top, width=30, font=("Arial", 18), show="*")
+my_pass.pack()
+home_pass = tkinter.Button(top, width=30, padx=2, pady=2, font=("Arial", 18), text="Print", command=lambda: my_print())
+home_pass.pack()
 top.mainloop()
